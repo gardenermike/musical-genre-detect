aac6abc60869ad1826b34e4b4e19cc6521162a1d
Mike Challis <mike.challis@matchbin.com>
2015-01-14 14:00:22 -0600

    Update ruby script to transcode before importing

--- execute.rb
+++ execute.rb
@@ -6,14 +6,16 @@ end
 
 genres.each_with_index do |genre, index|
   files = Dir.entries("#{base_directory}/#{genre}").select do |path|
-    File.file?("#{base_directory}/#{genre}/#{path}") && path.match(/wav$/)
+    File.file?("#{base_directory}/#{genre}/#{path}") && path.match(/mp3$/)
   end
 
   files.each do |file|
     path = "#{base_directory}/#{genre}/#{file}"
     puts "about to run #{path}"
-    system "octave", "-q", "transform.m", path, (index + 1).to_s
-    length = `wc -l X.txt`
-    puts length
+    wav_path = "#{path}.wav"
+    system "lame","--decode", "-b", "16","-m", "m", path, wav_path
+    system "octave", "-q", "transform.m", wav_path, (index + 1).to_s
+    #length = `wc -l X.txt`
+    #puts "X.txt is now #{length} lines long"
   end
 end
