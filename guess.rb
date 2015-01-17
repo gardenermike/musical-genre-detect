4820fea975cbdc31b446f6b92a47372b9c6229e6
Mike Challis <mike.challis@matchbin.com>
2015-01-17 11:14:56 -0600

    Update guess script to transcode

--- guess.rb
+++ guess.rb
@@ -5,8 +5,11 @@ base_directory = Pathname.new('music')
 genres = Dir.entries('music').select {|item| (base_directory + item).directory?}
 
 filename = ARGV[0]
+#convert file to wav
+wav_filename = filename + '.wav'
+system "lame","--decode", "-b", "16","-m", "m", filename, wav_filename
 
-Open3.popen3('octave', '-q', 'guess.m', filename) do |stdin, stdout, stderr, wait_thr|
+Open3.popen3('octave', '-q', 'guess.m', wav_filename) do |stdin, stdout, stderr, wait_thr|
 
   genre_id = stdout.gets
 
